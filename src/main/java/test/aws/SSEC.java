54129a6fb9e813c449aa752e4d3d52067a2015dd
Mateusz Mrozewski <matixo@gmail.com>
2019-01-05 20:39:32 +1100

    Initial import

---
+++ src/main/java/test/aws/SSEC.java
@@ -0,0 +1,52 @@
+package test.aws;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * Example of storing a file in S3 using AWS SDK v2 and
+ * customer provided encryption keys.
+ */
+public class SSEC {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256, new SecureRandom());
+        SecretKey secretKey = keyGenerator.generateKey();
+        String secretKeyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(secretKey.getEncoded());
+        byte[] digest = md5.digest();
+        String md5String = Base64.getEncoder().encodeToString(digest);
+
+        S3Client client = S3Client.builder().region(Region.AP_SOUTHEAST_2).build();
+
+        PutObjectRequest putRequest = PutObjectRequest.builder()
+                .bucket("my-encrypted-bucket-ssec")
+                .key("my-file.png")
+                .sseCustomerAlgorithm("AES256")
+                .sseCustomerKey(secretKeyString)
+                .sseCustomerKeyMD5(md5String)
+                .build();
+        client.putObject(putRequest, Paths.get("my-file.png"));
+
+        GetObjectRequest getRequest = GetObjectRequest.builder()
+                .bucket("my-encrypted-bucket-ssec")
+                .key("my-file.png")
+                .sseCustomerAlgorithm("AES256")
+                .sseCustomerKey(secretKeyString)
+                .sseCustomerKeyMD5(md5String)
+                .build();
+        client.getObject(getRequest, Paths.get("my-file-copy.png"));
+    }
+}
